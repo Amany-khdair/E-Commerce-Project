66bbab00de9e026e262488cbecd268122b9eb497
Amany-khdair <amany.khdair0@gmail.com>
2025-12-15 16:26:44 +0200

    make the default url open at home page

--- src/route.jsx
+++ src/route.jsx
@@ -18,6 +18,9 @@ const router = createBrowserRouter([
     path: "/",
     element: <MainLayout/>,
     children:[
+        { index: true, 
+          element: <Home /> 
+        },
         {
           path: 'home',
           element: <Home/>
