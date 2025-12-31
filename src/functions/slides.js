7c254960546d8aafa0ec5e5044c6c18c3df62342
Amany-khdair <amany.khdair0@gmail.com>
2025-12-31 23:45:01 +0200

    moved carousel images from src to public(try #2)

--- src/functions/slides.js
+++ src/functions/slides.js
@@ -4,24 +4,24 @@ export const slides = [
     id: 1,
     title: "iPhone 14 Series",
     subtitle: "Up to 10% off Voucher",
-    img: "/public/carosel/phone.webp",     
+    img: "/carosel/phone.webp",     
   },
   {
     id: 2,
     title: "MacBook Pro 2025",
     subtitle: "Get 15% off now",
-    img: "/public/carosel/Mac.webp",   
+    img: "/carosel/Mac.webp",   
   },
   {
     id: 3,
     title: "Apple Watch Series 9",
     subtitle: "Limited time offer",
-    img: "/public/carosel/AppleWatch.webp",     
+    img: "/carosel/AppleWatch.webp",     
   },
   {
     id: 4,
     title: "Air fryer 8 litres",
     subtitle: "Special Discount",
-    img: "/public/carosel/AirFlayer.webp",    
+    img: "/carosel/AirFlayer.webp",    
   },
 ];
