9b28e35291149e0042ff5843247b3c414904db7f
Amany-khdair <amany.khdair0@gmail.com>
2026-01-21 21:28:13 +0200

    moved slides images from public to assets

--- src/functions/slides.js
+++ src/functions/slides.js
@@ -1,31 +1,37 @@
+import appleLogo from '../assets/images/carosel/Apple_logo.webp';
+import phoneImg from '../assets/images/carosel/phone.webp';
+import macImg from '../assets/images/carosel/Mac.webp';
+import watchImg from '../assets/images/carosel/AppleWatch.webp';
+import airImg from '../assets/images/carosel/AirFlayer.webp';
 
 export const slides = [
   {
     id: 1,
     title: "iPhone14Series",
     subtitle: "UOV",
-    img: "/carosel/phone.webp",  
-    logo: "/carosel/Apple_logo.webp",   
+    img: phoneImg,  
+    logo: appleLogo,   
   },
   {
     id: 2,
     title: "MacBookPro2025",
     subtitle: "GON",
-    img: "/carosel/Mac.webp",  
-    logo: "/carosel/Apple_logo.webp",   
+    img: macImg,  
+    logo: appleLogo,   
   },
   {
     id: 3,
     title: "AppleWatchSeries9",
     subtitle: "LTO",
-    img: "/carosel/AppleWatch.webp",   
-    logo: "/carosel/Apple_logo.webp",    
+    img: watchImg,   
+    logo: appleLogo,    
   },
   {
     id: 4,
     title: "AirFryerLitres",
     subtitle: "SD",
-    img: "/carosel/AirFlayer.webp",  
+    img: airImg,  
     logo: "",    
   },
 ];
+
